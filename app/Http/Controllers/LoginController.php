81489be3819bdc9187a49794a9fceff4296558cf
shoaib2018 <shoaibhaque7@gmail.com>
2021-01-02 13:05:47 +0600

    patch-login-controller-index-route

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LoginController extends Controller
+{
+    public function index(){
+    	return view('login.index');
+    }
+
+    public function login(Request $req){
+    	if(count(DB::table('students')->where('name',$req->sudentname)->where('password',$req->password)->first())>0){
+    		return redirect()->route('home.index');
+    	}
+    	else{
+    		$req->session()->flash('msg', 'Invalid Name or Password!');
+    		return redirect()->route('login.index');
+    	}
+    }
+}
